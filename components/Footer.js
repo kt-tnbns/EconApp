fbae0f3bd77c895a9005b28e240d2b30c21adac1
Tanabut <tangg6@outlook.com>
2022-08-27 00:28:22 +0700

    Add Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,23 @@
-import { StyleSheet,} from 'react-native'
-import React from 'react'
-import { NativeBaseProvider, Box, Text, Heading, VStack, FormControl, Input, Link, Button, Icon, HStack, Center, Pressable } from 'native-base';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import {StyleSheet} from 'react-native';
+import React from 'react';
+import {
+  NativeBaseProvider,
+  Box,
+  Text,
+  Heading,
+  VStack,
+  FormControl,
+  Input,
+  Link,
+  Button,
+  Icon,
+  HStack,
+  Center,
+  Pressable,
+} from 'native-base';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import {FooterData} from './FooterData';
 
 const Footer = () => {
   const [selected, setSelected] = React.useState(1);
@@ -11,46 +26,40 @@ const Footer = () => {
     <NativeBaseProvider>
       <Box flex={1} bg="white" safeAreaTop width="100%" alignSelf="center">
         <Center flex={1}></Center>
-        <HStack bg="indigo.600" alignItems="center" safeAreaBottom shadow={6}>
-          <Pressable cursor="pointer" opacity={selected === 0 ? 1 : 0.5} py="3" flex={1} onPress={() => setSelected(0)}>
-            <Center>
-              <Icon mb="1" as={<MaterialCommunityIcons name={selected === 0 ? 'home' : 'home-outline'} />} color="white" size="sm" />
-              <Text color="white" fontSize="12">
-                Home
-              </Text>
-            </Center>
-          </Pressable>
-          <Pressable cursor="pointer" opacity={selected === 1 ? 1 : 0.5} py="2" flex={1} onPress={() => setSelected(1)}>
-            <Center>
-              <Icon mb="1" as={<MaterialIcons name="search" />} color="white" size="sm" />
-              <Text color="white" fontSize="12">
-                Search
-              </Text>
-            </Center>
-          </Pressable>
-          <Pressable cursor="pointer" opacity={selected === 2 ? 1 : 0.6} py="2" flex={1} onPress={() => setSelected(2)}>
-            <Center>
-              <Icon mb="1" as={<MaterialCommunityIcons name={selected === 2 ? 'cart' : 'cart-outline'} />} color="white" size="sm" />
-              <Text color="white" fontSize="12">
-                Cart
-              </Text>
-            </Center>
-          </Pressable>
-          <Pressable cursor="pointer" opacity={selected === 3 ? 1 : 0.5} py="2" flex={1} onPress={() => setSelected(3)}>
-            <Center>
-              <Icon mb="1" as={<MaterialCommunityIcons name={selected === 3 ? 'account' : 'account-outline'} />} color="white" size="sm" />
-              <Text color="white" fontSize="12">
-                Account
-              </Text>
-            </Center>
-          </Pressable>
+
+        <HStack height="60" bg="primary.50" alignItems="center" safeAreaBottom shadow={2}>
+          {FooterData.map((item, index) => {
+            const IconType = item.iconType
+            return (
+              <Pressable
+                cursor="pointer"
+                opacity={selected === index ? 1 : 0.5}
+                py="2"
+                flex={1}
+                onPress={() => setSelected(index)}
+                key={item.title}>
+                <Center>
+                  <Icon
+                    mb="1"
+                    as={
+                      <IconType
+                        name={selected === index ? item.iconSelected : item.iconUnselect}
+                      />
+                    }
+                    color="primary.700"
+                    size="8"
+                  />
+
+                </Center>
+              </Pressable>
+            );
+          })}
         </HStack>
       </Box>
     </NativeBaseProvider>
-    
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
---
+++ components/FooterData.js
@@ -0,0 +1,36 @@
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+export const FooterData = [
+    {
+        title:'Home',
+        iconType:MaterialCommunityIcons,
+        iconSelected:'home',
+        iconUnselect : 'home',
+
+    },
+    {
+        title:'GoldPrice',
+        iconType:MaterialCommunityIcons,
+        iconSelected:'gold',
+        iconUnselect : 'gold'
+    },
+    {
+        title:'OilPrice',
+        iconType:MaterialCommunityIcons,
+        iconSelected:'gas-station',
+        iconUnselect : 'gas-station'
+    },
+    {
+        title:'Currency',
+        iconType:MaterialCommunityIcons,
+        iconSelected:'currency-usd',
+        iconUnselect :'currency-usd'
+    },
+    {
+        title:'GDP',
+        iconType:MaterialCommunityIcons,
+        iconSelected:'chart-bar',
+        iconUnselect : 'chart-bar'
+    }
+]
